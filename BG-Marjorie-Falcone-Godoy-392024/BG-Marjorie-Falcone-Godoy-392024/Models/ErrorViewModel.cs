bf2e419bba3e55b86997a30c821657377ac186c7
MarjorieFalconeGod <marjorie.falcone@sasf.net>
2024-09-03 10:01:50 -0500

    Inicializacion del sistema

---
+++ BG-Marjorie-Falcone-Godoy-392024/BG-Marjorie-Falcone-Godoy-392024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BG_Marjorie_Falcone_Godoy_392024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
